5a7d8add759c62475ba3dcc248f5894f123e3809
Aarni Alasaarela <aarni@heychain.io>
2023-11-12 02:50:52 +0200

    add cors policy

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,6 +5,7 @@ load_dotenv(".env")
 import logging
 import logging.config
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.config import settings, log_config
 from src.api.v1.api import api_router
 
@@ -13,6 +14,21 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_router, prefix=settings.API_V1_ENDPOINT)
+
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health/")
